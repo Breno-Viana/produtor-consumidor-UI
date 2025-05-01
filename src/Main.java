153f1477c3f8e5b4ba81440a74069f09e10873bc
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2025-05-01 00:29:16 -0300

    segunda tela criada

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import utils.Buffer;
-import utils.ProduceAndConsume;
+import javafx.stage.Window;
+import stages.InitializerStage;
 
 public class Main extends Application {
-    private Buffer<?> buffer;
-    private ProduceAndConsume PC;
     private final double containerHeight = 650;
     private final double containerWidth = 1100;
-
+    private final InitializerStage initializerStage = new InitializerStage();
     private final String defaultButtonStyle = "-fx-background-color: #000;" +
             "-fx-min-width: 200px;" +
             "-fx-min-height: 100px;" +
@@ -29,44 +27,22 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         AnchorPane root = new AnchorPane();
-
         Scene scene = new Scene(root, containerWidth, containerHeight);
 
-        Button startButton = new Button("Start");
-        //  startButton.setPrefSize(200, 100);
-
-        startButton.getStyleClass().add("btn");
-        startButton.setStyle(defaultButtonStyle);
-
-
-        startButton.setOnMouseEntered(e ->{
-            startButton.setStyle(hoverButtonStyle);
-        });
-        startButton.setOnMouseExited(e ->{
-            startButton.setStyle(defaultButtonStyle);
-        });
-        //startButton.setLayoutX(450);
-        //startButton.setLayoutY(450);
-
-
-        startButton.setOnAction(event -> {
-            System.out.println("Start");
-        });
+        Label ST = new Label("Podutor e Consumidor");
+        Button startButton = getStartButton(stage);
 
         AnchorPane.setTopAnchor(startButton, (containerHeight-200));
         AnchorPane.setLeftAnchor(startButton, (containerWidth-200)/2);
 
-        root.getChildren().add(startButton);
 
-
-        Label ST = new Label("Podutor e Consumidor");
 
         ST.setStyle(labelStyle);
         AnchorPane.setTopAnchor(ST, (containerHeight-200)/2);
         AnchorPane.setLeftAnchor(ST, (containerWidth-500)/2);
-        root.getChildren().add(ST);
 
 
+        root.getChildren().addAll(ST, startButton);
         stage.setFullScreen(false);
         stage.setResizable(false);
         stage.setScene(scene);
@@ -74,11 +50,24 @@ public class Main extends Application {
     }
 
 
-    private void initializeBuffer(int capacity) {
-        System.out.println("Inicializando Buffer...");
-        buffer = new Buffer<>(capacity);
-        System.out.println("Buffer carregado com a capacidade " + capacity);
-    }
+    private Button getStartButton(Stage stage) {
+        Button startButton = new Button("Start");
+        startButton.setStyle(defaultButtonStyle);
+
+        startButton.setOnMouseEntered(e ->{
+            startButton.setStyle(hoverButtonStyle);
+        });
+        startButton.setOnMouseExited(e ->{
+            startButton.setStyle(defaultButtonStyle);
+        });
+
+        startButton.setOnAction(e ->{
+            Window window = stage.getScene().getWindow();
+            initializerStage.initialize((Stage) window);
+        }); return startButton;
+    };
+
+
 
 
 }
---
+++ src/stages/InitializerStage.java
@@ -0,0 +1,97 @@
+package stages;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import utils.Buffer;
+import utils.ProduceAndConsume;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class InitializerStage {
+    private Buffer<?> buffer;
+    private ProduceAndConsume produceAndConsume;
+    private final double containerHeight = 650;
+    private final double containerWidth = 1100;
+
+
+    private final String sliderStyle = "-fx-min-width: 400px;" +
+            "-fx-min-height: 200px;" +
+            "-fx-font-size: 20px;";
+    private final String labelStyle = "-fx-font-size: 40px;";
+
+    private final String buttonStyle = " -fx-background-color: #000;"
+            + "-fx-min-width: 200px;"
+            + "-fx-min-height: 100px;"
+            + "-fx-text-fill: #fff;"
+            + "-fx-font-size: 26px;";
+    private final String hoverStyle = buttonStyle +
+            "-fx-background-color: #fff;" +
+            "-fx-text-fill: #000;" +
+            "-fx-cursor: hand;" +
+            "-fx-effect: dropshadow(gaussian, rgba(0,0,0,0.5), 10, 0.5, 0, 0);";
+
+    public void initialize(Stage stage) {
+        String labelText = "Escolha o capacidade da fila/buffer: ";
+        AtomicInteger capacity = new AtomicInteger(5);
+        AnchorPane root = new AnchorPane();
+        root.setPrefSize(containerWidth, containerHeight);
+        Scene scene = new Scene(root);
+
+        Slider slider = getSlider();
+        Label label = new Label(labelText);
+        Button initialize = getInitialize(capacity);
+
+        AnchorPane.setLeftAnchor(slider, (containerWidth - 410) / 2);
+        AnchorPane.setTopAnchor(slider, (containerHeight - 200) / 2);
+
+        AnchorPane.setLeftAnchor(label, (containerWidth - 650) / 2);
+        AnchorPane.setTopAnchor(label, (containerHeight - 400) / 2);
+
+        AnchorPane.setTopAnchor(initialize, (containerHeight - 200));
+        AnchorPane.setLeftAnchor(initialize, (containerWidth - 250) / 2);
+
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            capacity.set(newValue.intValue());
+            label.setText(labelText + capacity.get());
+
+        });
+        label.setStyle(labelStyle);
+
+        root.getChildren().addAll(label, initialize, slider);
+
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private Button getInitialize(AtomicInteger capacity) {
+        Button initialize = new Button("Clique para Iniciar");
+        initialize.setStyle(buttonStyle);
+        initialize.setOnMouseEntered(e -> {
+            initialize.setStyle(hoverStyle);
+        });
+        initialize.setOnMouseExited(e -> {
+            initialize.setStyle(buttonStyle);
+        });
+        initialize.setOnAction(e -> {
+            if (capacity.get() == 0 || capacity.get() < 5) {
+                System.out.println("Erro ao iniciar");
+            }else System.out.println("Iniciado "+ capacity.get());
+        });
+        return initialize;
+    }
+
+
+    private Slider getSlider() {
+        Slider slider = new Slider(5, 20, 1);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+        slider.setMajorTickUnit(1);
+        slider.setStyle(sliderStyle);
+        return slider;
+    }
+
+
+}
